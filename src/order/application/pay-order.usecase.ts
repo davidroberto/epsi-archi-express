c4cd237e429e3ce6eb2d5002c2772f8bedd84cdc
David Robert <contact@david-robert.fr>
2025-01-15 16:02:03 +0100

    inject repository in use case via constructor

--- src/order/application/pay-order.usecase.ts
+++ src/order/application/pay-order.usecase.ts
@@ -1,11 +1,23 @@
 import Order from "../domain/order.entity";
+import OrderRepository from "../infrastructure/order.repository";
 import { OrderContainer } from "../order.container";
 
 export class PayOrderUseCase {
-  payOrder(orderId: number): Order {
-    const orderRepository = OrderContainer.getOrderRepository();
+  // je créé une propriété privée orderRepository de type OrderRepository
+  private orderRepository: OrderRepository;
 
-    const order = orderRepository.findById(orderId);
+  // je créé un constructeur, qui est appelé automatiquement
+  // quand la classe PayOrderUseCase est instanciée
+  // mon constructeur ici récupère l'instance de OrderRepository
+  // depuis le container
+  // et la stocke dans la propriété orderRepository
+  constructor() {
+    this.orderRepository = OrderContainer.getOrderRepository();
+  }
+
+  payOrder(orderId: number): Order {
+    // j'utilise la propriété orderRepository
+    const order = this.orderRepository.findById(orderId);
 
     if (!order) {
       throw new Error("Order not found");
